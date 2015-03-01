178da070bf6a9cd93ed6dba6efe74bdcd67ea12a
iamthedugas <iamthedugas@gmail.com>
2015-03-01 13:47:11 -0500

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Creddaspot::Application.config.secret_key_base = '44a107e5bbb76d561a0a436ccb41f69e3969e5036e99587305ae030fa78dac64d3edd233f6f40e416b3ed000e3aab3d758e48726a4d96563835362492e9e4f20'
